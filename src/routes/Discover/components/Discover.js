bb4843f35e42752c09eea7a63826bbf559735b38
Barbara Diaz <barbarad91@gmail.com>
2021-04-12 11:49:00 +0200

    Add newReleases data

--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,27 +1,57 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from 'react'
+import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock'
+import '../styles/_discover.scss'
+import axios from 'axios'
 
 export default class Discover extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
-    };
+      categories: [],
+    }
+
+    this.baseUrl = 'https://api.spotify.com'
+  }
+
+  fetchNewReleases = async () =>
+    await axios.get(`${this.baseUrl}/v1/browse/new-releases`, {
+      headers: {
+        Authorization:
+          'Bearer BQAuSmuxPJTNaExpOjEg9pBVjGNhyLy3I4stzFJspXFa9EGM0-s7_ItuMZDCr0dheYDIH6PEpW7KFPPSJAKyh9i6Siqq-lxllmyZnx63Lpn9z1wfCPNG05O1PajVmrY9C93GpTRY7mefPGI',
+        'Content-Type': 'application/json',
+      },
+    })
+
+  componentDidMount = async () => {
+    const newReleases = await this.fetchNewReleases()
+    this.setState({ newReleases: newReleases.data.albums.items })
   }
 
   render() {
-    const { newReleases, playlists, categories } = this.state;
+    const { newReleases, playlists, categories } = this.state
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
-    );
+    )
   }
 }
